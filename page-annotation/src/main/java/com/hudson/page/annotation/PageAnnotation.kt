fc0f6c2cce8070875e6305c4db8e3551daa8af7f
Hudson <2216274479@qq.com>
2022-08-13 08:27:32 +0800

    create page annotation

---
+++ page-annotation/src/main/java/com/hudson/page/annotation/PageAnnotation.kt
@@ -0,0 +1,15 @@
+package com.hudson.page.annotation
+
+@Target(AnnotationTarget.TYPE)
+@Retention(AnnotationRetention.SOURCE)
+annotation class PageAnnotation(
+    /**
+     * Desc for page register on [targetHostPage] page.
+     */
+    val pageDesc: String,
+
+    /**
+     * The name of the host page which you want to register on.
+     */
+    val targetHostPage: String = "DefaultHostPage"
+)
